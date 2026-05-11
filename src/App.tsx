208c84b177fdb0a017b2b95d676f25f5f13a2d3a
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2026-05-12 00:35:57 +0100

    change contact email

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,7 @@ export default function App() {
             }
           />
           <SocialButton
-            href="mailto:liliana.lessa1@gmail.com"
+            href="mailto: contact@lilianalessa.dev"
             label={t.email}
             icon={
               <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} width={18} height={18}>
